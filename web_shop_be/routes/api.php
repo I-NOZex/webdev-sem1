5d1eb552d4792c9e6cd3b28772512a43f26c3ce2
I-NOZex <tiagofm.profissional@outlook.pt>
2021-12-02 20:23:24 +0100

    Color controller

--- web_shop_be/routes/api.php
+++ web_shop_be/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('colors' , Shop\ColorController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
