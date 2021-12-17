70f0901b130ecb06a71d677819e99e06d75c6667
I-NOZex <tiagofm.profissional@outlook.pt>
2021-12-17 16:00:34 +0100

    layout tidy

--- web_shop_fe/src/js/app.js
+++ web_shop_fe/src/js/app.js
@@ -10,6 +10,24 @@ import EventBus from './event-bus.js';
 
 const APP_CONTAINER = document.getElementById('app');
 const ENABLE_CACHING = false;
+const BASE_API_URL = 'http://127.0.0.1:8008';
+
+const fetchRemoteModel = async(contentUrl, model, ignoreCache = false) => {    
+    const absoluteUrl = BASE_API_URL + contentUrl;
+    
+    if (window.localStorage.getItem(absoluteUrl) !== null && !ignoreCache) {
+        return JSON.parse(window.localStorage.getItem(absoluteUrl));
+    }
+    console.log('fetching remote model')
+
+    return await fetch(absoluteUrl)
+    // The API call was successful!
+    .then(async (response) => ({[model] : await response.json()}))
+    .then((json) => {
+        window.localStorage.setItem(absoluteUrl, JSON.stringify(json));
+        return json;
+    })
+}
 
 let activeFilters = {
     categories : {
@@ -23,7 +41,7 @@ const routes = {
     '/' : {
         path: '/',
         template: '/_products-home.html',
-        remoteModel: async() => await fetchRemoteModel('http://localhost:8008/products', 'products'),
+        remoteModel: async() => await fetchRemoteModel('/products', 'products', true),
         staticModel: {
             user_name: 'Tiago',
             labelTitle: 'test title',
@@ -46,7 +64,7 @@ const routes = {
             filters: {
                 categories : [
                     { id: 'filter-cat-shirt', label: 'T-shirts', active: true, filterArg: 'price_gte=1'},
-                    { id: 'filter-cat-hoodie', label: 'Hoodies', active: false, filterArg: 'price_gte=10'},
+                    { id: 'filter-cat-hoodie', label: 'Hoodies', active: false, filterArg: 'price_gte=7'},
                     { id: 'filter-cat-sweat', label: 'Sweaters', active: false, filterArg: 'price_gte=15'},
                 ],
 
@@ -59,8 +77,64 @@ const routes = {
             //resultCount: 0,
         },
         
-        remoteModel: async() => await fetchRemoteModel('http://localhost:8008/products?price_gte=15', 'product')
+        remoteModel: async() => await fetchRemoteModel('/products-filter?price_gte=1', 'products', true)
     },
+
+    '/products?hoodies' : {
+        path: '/products[?]hoodies',
+        template: '/_products-list.html',
+        staticModel: {
+            user_name: 'Tiago',
+            labelTitle: 'test title',
+            xpto: 'a xpto val',
+            xpto2: 'a xpto2 title',
+            filters: {
+                categories : [
+                    { id: 'filter-cat-shirt', label: 'T-shirts', active: false, filterArg: 'price_gte=1'},
+                    { id: 'filter-cat-hoodie', label: 'Hoodies', active: true, filterArg: 'price_gte=7'},
+                    { id: 'filter-cat-sweat', label: 'Sweaters', active: false, filterArg: 'price_gte=15'},
+                ],
+
+                price: [
+                    { label: 'caro'},
+                    { label: 'carote'},
+                    { label: 'barato'},
+                ],
+            },
+            //resultCount: 0,
+        },
+        
+        remoteModel: async() => await fetchRemoteModel('/products-filter?price_gte=7', 'products', true)
+    },    
+
+
+    '/products?sweaters' : {
+        path: '/products[?]sweaters',
+        template: '/_products-list.html',
+        staticModel: {
+            user_name: 'Tiago',
+            labelTitle: 'test title',
+            xpto: 'a xpto val',
+            xpto2: 'a xpto2 title',
+            filters: {
+                categories : [
+                    { id: 'filter-cat-shirt', label: 'T-shirts', active: false, filterArg: 'price_gte=1'},
+                    { id: 'filter-cat-hoodie', label: 'Hoodies', active: false, filterArg: 'price_gte=7'},
+                    { id: 'filter-cat-sweat', label: 'Sweaters', active: true, filterArg: 'price_gte=15'},
+                ],
+
+                price: [
+                    { label: 'caro'},
+                    { label: 'carote'},
+                    { label: 'barato'},
+                ],
+            },
+            //resultCount: 0,
+        },
+        
+        remoteModel: async() => await fetchRemoteModel('/products-filter?price_gte=15', 'products', true)
+    },
+
 
     '/products/[0-9]' : {
         path: '/products/(?<id>[0-9]+)',
@@ -76,24 +150,8 @@ const routes = {
             //resultCount: 0,
         },
         
-        remoteModel: async() => await fetchRemoteModel(`http://localhost:8008/products/${Number.parseInt(window.history.state.id)}`, 'product')
+        remoteModel: async() => await fetchRemoteModel(`/products/${Number.parseInt(window.history.state.id)}`, 'product')
     },
-}
-
-
-const fetchRemoteModel = async(contentUrl, model, ignoreCache = false) => {     
-    if (window.localStorage.getItem(contentUrl) !== null && !ignoreCache) {
-        return JSON.parse(window.localStorage.getItem(contentUrl));
-    }
-    console.log('fetching remote model')
-
-    return await fetch(contentUrl)
-    // The API call was successful!
-    .then(async (response) => ({[model] : await response.json()}))
-    .then((json) => {
-        window.localStorage.setItem(contentUrl, JSON.stringify(json));
-        return json;
-    })
 }
 
 const EVENTBUS = window.EVENTBUS = new EventBus();
@@ -108,12 +166,12 @@ const App = new VanillaSpaEngine({
 const filter = (e) => {
 //console.log(e)
  App.updateCurrentModel({
-    remoteModel: async() => await fetchRemoteModel(`http://localhost:8008/products-filter?${e.detail.args}`, 'products',true),
+    remoteModel: async() => await fetchRemoteModel(`/products-filter?${e.detail.args}`, 'products', true),
     staticModel: {
         filters: {
             categories: [
                 { id: 'filter-cat-shirt', label: 'T-shirts', active: (e.detail.args === 'price_gte=1'), filterArg: 'price_gte=1'},
-                { id: 'filter-cat-hoodie', label: 'Hoodies', active: (e.detail.args === 'price_gte=10'), filterArg: 'price_gte=10'},
+                { id: 'filter-cat-hoodie', label: 'Hoodies', active: (e.detail.args === 'price_gte=7'), filterArg: 'price_gte=7'},
                 { id: 'filter-cat-sweat', label: 'Sweaters', active: (e.detail.args === 'price_gte=15'),  filterArg: 'price_gte=15'},
             ],
        },
